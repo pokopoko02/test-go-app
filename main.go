38829e33bb57246dfe74efe92f4669cd60ca696c
pokopoko02 <r.nagai@creativesurvey.com>
2023-10-17 10:32:25 +0900

    update main.go & main_test.go

--- main.go
+++ main.go
@@ -5,13 +5,15 @@ import (
 	"net/http"
 )
 
+func RootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello world!")
+}
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "ok")
+}
 func main() {
 	log.Println("start")
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello world!")
-	})
-	http.HandleFunc("/health_check", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "ok")
-	})
+	http.HandleFunc("/", RootHandler)
+	http.HandleFunc("/health_check", HealthCheck)
 	log.Fatal(http.ListenAndServe(":8080", nil))
-	}
+}
