e78d6ccbe2a4da1fe429e8e44d394b77566d3f67
pokopoko02 <r.nagai@creativesurvey.com>
2023-10-17 10:12:47 +0900

    test git ci

--- main.go
+++ main.go
@@ -5,13 +5,15 @@ import (
 	"net/http"
 )
 
-func main() {
-	log.Println("start")
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+func RootHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello world!")
-	})
-	http.HandleFunc("/health_check", func(w http.ResponseWriter, r *http.Request) {
+	}
+	func HealthCheck(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "ok")
-	})
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	}
+	func main() {
+		log.Println("start")
+		http.HandleFunc("/", RootHandler)
+		http.HandleFunc("/health_check", HealthCheck)
+		log.Fatal(http.ListenAndServe(":8080", nil))
 	}
