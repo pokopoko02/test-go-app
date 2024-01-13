891b4bc1947ee43e600811bae6519dd5cbd34752
pokopoko02 <r.nagai@creativesurvey.com>
2024-01-14 05:58:08 +0900

    minor revise

--- main.go
+++ main.go
@@ -4,9 +4,8 @@ import (
 	"log"
 	"net/http"
 )
-
 func RootHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello world!")
+	fmt.Fprint(w, "Hello world! Welcome Go App!")
 }
 func HealthCheck(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "ok")
